22a8e1ab9b65deb61d44f347f09e3fef3a7de867
Glauco Todesco <glauco.todesco@gmail.com>
2021-11-10 15:50:39 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+/************ CHANGE TO YOUR BUCKET NAME ***************/
+const s3BucketName = 'test-s3-lambda-glauco';
+
+const save = {
+    async write(data, fileName, bucket) {
+        
+        const params = {
+            Bucket: bucket,
+            Body: JSON.stringify(data),
+            Key: fileName,
+        };
+        
+        const newData = await s3.putObject(params).promise();
+      
+        if (!newData) {
+            throw Error('there was an error writing the file');
+        }
+        
+        return newData;
+    },
+};
+
+exports.handler = async (event) => {
+
+    var query = event["queryStringParameters"];
+    var data;
+    
+    if(query)
+    {
+         var value = query['value'];
+         if(value){
+            const newData = await save.write(JSON.stringify(value),"file.txt",s3BucketName);
+            data = JSON.stringify(newData);
+         }
+         else{
+             data = JSON.stringify('Nome da query inválida! Passe uma query: ?value=XXXXXXXXX');
+         }
+    }
+    else{
+        data = JSON.stringify('Passe uma query: ?value=XXXXXXXXX');
+    }
+    
+    const response = {
+        statusCode: 200,
+        body: data
+    };
+    
+
+    return response;
+};
